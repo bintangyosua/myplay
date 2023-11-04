daab5ce712cf3d532bcf7f64521f5b9a92cb7921
Bintang <animerevindo@gmail.com>
2023-11-04 09:59:13 +0700

    1/2 secure

---
+++ app/controllers/Security.java
@@ -0,0 +1,10 @@
+package controllers;
+
+public class Security extends controllers.Secure.Security {
+    public static boolean authenticate(String username, String password) {
+        boolean boleh = false;
+
+        return boleh;
+    }
+    
+}
--- app/controllers/buku_alamat.java
+++ app/controllers/buku_alamat.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 import models.*;
 
+@With(Secure.class)
 public class buku_alamat extends Controller {
 
     public static void index() {
