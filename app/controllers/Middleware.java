b03fc5466b09d827dd88cac46097e63581f0623c
Bintang <animerevindo@gmail.com>
2023-11-11 23:04:15 +0700

    removed javadoc dir and failed to try casaced

--- app/controllers/Middleware.java
+++ app/controllers/Middleware.java
@@ -4,8 +4,6 @@ import models.Karya;
 
 public class Middleware {
     public static void checkKarya(String profile, Karya karya) {
-        System.out.println("profile: " + profile);
-        System.out.println("karya.user.username: " + karya.user.username);
         if (!profile.equals(karya.user.username)) {
             Application.home();
         }
