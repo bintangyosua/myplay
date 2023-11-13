7453a98a0ee48cdb2ab481b4c4832cb2d21edc3a
Bintang <animerevindo@gmail.com>
2023-11-13 19:06:23 +0700

    minor problem edited

---
+++ insert_data.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 13, 2023 at 12:01 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pbo_project`
+--
+
+--
+-- Dumping data for table `episode`
+--
+
+INSERT INTO `episode` (`id`, `cerita`, `judul`, `no_eps`, `karya_id`) VALUES
+(24, 'Di sebuah desa kecil yang diapit oleh hamparan bunga matahari yang indah, terdapat sebuah keajaiban yang disebut Pohon Ajaib. Desa ini dikenal sebagai Desa Sunflower, dan Pohon Ajaib menjadi pusat perhatian para penduduknya.\r\n\r\nPohon Ajaib itu sendiri memiliki batang yang berkilauan seperti emas dan daun-daun yang berwarna-warni seperti pelangi. Setiap daunnya memiliki daya magis yang memancarkan keberuntungan dan kebahagiaan ke seluruh desa. Konon, kata-kata bijak dan keinginan baik yang diucapkan di dekat Pohon Ajaib akan terpatri dalam daun-daunnya dan menjadi kenyataan.\r\n\r\nDi tengah desa, hidup seorang gadis muda bernama Luna. Luna adalah seorang pemimpi dan selalu tertarik pada keindahan alam. Suatu hari, ketika ia sedang duduk di bawah Pohon Ajaib, dia mulai bermimpi besar. Dia berharap dapat membantu desanya tumbuh dan berkembang.\r\n\r\nKeesokan harinya, Luna memutuskan untuk mengajak penduduk desa untuk bekerja sama dalam menjaga dan merawat Pohon Ajaib. Bersama-sama, mereka membersihkan area sekitar pohon, menyirami tanah, dan memberikan perawatan khusus. Pohon Ajaib tampaknya merespons dengan gemuruh lembut dan kilauan yang semakin bersinar.\r\n\r\nSeiring waktu, Desa Sunflower benar-benar berubah. Tanaman menjadi lebih subur, dan bunga matahari berkembang lebih indah. Penduduk desa menjadi lebih bahagia dan penuh semangat. Keajaiban Pohon Ajaib benar-benar terasa, dan orang-orang mulai mengakui peran Luna dalam perubahan ini.\r\n\r\nLuna terus menjalani kehidupannya di Desa Sunflower dengan penuh kebahagiaan. Dia menjadi inspirasi bagi orang-orang di sekitarnya untuk selalu bermimpi besar dan bekerja sama dalam menciptakan keindahan dan kebahagiaan. Dan Pohon Ajaib di Desa Sunflower tetap menjadi saksi bisu dari setiap keinginan baik dan harapan yang diucapkan oleh penduduk desa.', 'Pohon Ajaib di Desa Sunflower', 1, 23),
+(27, '\r\nDi zaman yang lampau, di sebuah kerajaan di pedalaman gurun yang luas, hiduplah seorang pangeran muda yang bernama Amir. Amir adalah pewaris takhta yang berhati mulia dan penuh tekad. Kerajaan ini dikenal karena kemakmurannya dan keindahan istananya yang megah.\r\n\r\nAmir tumbuh menjadi pangeran yang bijaksana dan penuh kepedulian terhadap rakyatnya. Ia tidak hanya terpaku pada kemewahan istana, tetapi sering turun langsung ke pasar dan desa untuk berbicara dengan rakyatnya. Semua orang mengagumi keadilan dan kebaikan hati Amir.\r\n\r\nSuatu hari, seorang utusan dari kerajaan tetangga datang membawa berita tentang ancaman perang. Amir merasa terpanggil untuk mencari solusi yang damai. Dengan penuh bijaksana, ia memutuskan untuk mengunjungi penguasa kerajaan tetangga dan berusaha menjalin perdamaian.\r\n\r\nDalam perjalanannya, Amir melewati gurun yang luas dan penuh tantangan. Ia tidak pernah sendiri, selalu ditemani oleh pasukan setia dan penasehat bijaksana. Namun, di tengah perjalanan, Amir bertemu dengan seorang pedagang tua yang terdampar di padang pasir.\r\n\r\nPedagang itu menceritakan kisahnya yang menyedihkan, kehilangan segala harta dan keluarganya dalam badai pasir yang dahsyat. Amir merasa belas kasihan dan memutuskan untuk membantu. Ia memberikan tempat di dalam kelompoknya kepada pedagang itu dan membagi rezekinya.\r\n\r\nKetika akhirnya Amir tiba di kerajaan tetangga, ia diterima dengan baik oleh penguasa setempat. Amir berbicara tentang kedamaian, keadilan, dan kerjasama. Melalui perundingan yang bijaksana, Amir berhasil mencapai kesepakatan damai yang menguntungkan kedua kerajaan.\r\n\r\nPrestasinya membuat Amir dihormati bukan hanya di kerajaannya sendiri, tetapi juga di seluruh wilayah. Ia pulang dengan kepala tegak, membawa berita perdamaian dan harapan bagi rakyatnya.\r\n\r\nAmir tidak hanya menjadi pangeran yang bijaksana, tetapi juga menjadi pemimpin yang memahami nilai-nilai kemanusiaan. Kisahnya menjadi inspirasi bagi banyak generasi, mengajarkan bahwa keadilan, kebijaksanaan, dan kasih sayang adalah kunci untuk memimpin dengan bijaksana dan menciptakan perdamaian.', 'Pangeran Arab', 2, 23),
+(30, 'Pada suatu hari di dunia Naruto, terjadi sebuah kekacauan besar. Sebuah kelompok shinobi jahat yang dipimpin oleh seorang penjahat misterius bernama Kagokira mencoba untuk merebut kendali atas seluruh alam semesta ninja. Mereka memiliki rencana untuk memanipulasi kekuatan chakra dan menaklukkan dunia shinobi.\r\n\r\nMelihat ancaman ini, pemimpin desa-desa shinobi besar berkumpul untuk merencanakan perlawanan terhadap kelompok Kagokira. Namun, mereka menyadari bahwa kekuatan yang mereka hadapi jauh melampaui kapasitas kekuatan ninja biasa. Oleh karena itu, mereka memutuskan untuk mencari bantuan dari luar dunia mereka.\r\n\r\nMereka menghubungi seorang pahlawan yang terkenal di luar alam semesta ninja, seorang prajurit Saiyan yang memiliki kekuatan luar biasa bernama Goku. Di sisi lain, seorang bajak laut hebat dari dunia yang berbeda juga mendengar kabar tersebut. Dia adalah Monkey D. Luffy, kapten Bajak Laut Topi Jerami, yang dikenal karena kekuatannya yang berasal dari buah iblis.\r\n\r\nGoku dan Luffy tiba di dunia Naruto dan bersatu dengan para shinobi untuk menghadapi ancaman dari Kagokira. Pertemuan mereka menciptakan aliansi yang luar biasa antara tiga dunia yang berbeda. Goku membawa kekuatan serangan energi dan teknik bela diri Saiyan-nya, sementara Luffy menggunakan kekuatan buah iblisnya untuk merubah tubuhnya dan memberikan serangan yang luar biasa.\r\n\r\nPertempuran epik pun dimulai. Kagokira dan pasukannya yang kuat meluncurkan serangan demi serangan, tetapi aliansi Goku, Luffy, dan para shinobi tidak kenal lelah. Mereka menggabungkan kekuatan mereka dalam pertempuran yang mempesona, menggunakan teknik-teknik dan serangan yang belum pernah terlihat sebelumnya.\r\n\r\nSelama pertempuran, Goku dan Luffy saling memotivasi satu sama lain dan memacu para shinobi untuk memberikan yang terbaik. Setiap gerakan spektakuler dari Goku atau serangan elastis dari Luffy memberikan semangat baru pada para pejuang di sana.\r\n\r\nPertempuran mencapai puncaknya saat Goku, Luffy, dan para shinobi bersatu untuk menggabungkan kekuatan mereka dalam serangan gabungan yang menghancurkan rencana Kagokira. Setelah pertempuran sengit, Kagokira dan pasukannya terpaksa mengakui kekuatan dari aliansi luar biasa ini.\r\n\r\nSetelah kemenangan mereka, Goku dan Luffy memutuskan untuk kembali ke dunia masing-masing, meninggalkan dunia Naruto dengan damai dan merestorasi ketenangan di seluruh alam semesta ninja. Namun, cerita perlawanan kemerdekaan yang luar biasa ini akan dikenang oleh seluruh dunia, menjadi legenda yang menceritakan kekuatan persatuan dan keberanian dari tiga dunia yang berbeda.', 'Perang Kemerdekaan Luffy Terhadap Penjajah Goku', 3, 23),
+(37, 'Di sebuah desa kecil yang dikelilingi oleh hutan lebat, kehidupan berjalan dengan damai. Namun, pada suatu malam, langit tiba-tiba menggelap, dan petir menyambar tanpa henti. Penduduk desa yang gelisah mencari petunjuk tentang apa yang terjadi. Seorang tokoh misterius muncul, mengatakan bahwa langit sedang marah dan hanya dengan menemukan tiga artefak kuno, mereka bisa merestuasi ketenangan. Petualangan pun dimulai.', 'Awal Gelap', 1, 35),
+(38, 'Dua pahlawan, Mia dan Rian, dipilih untuk mencari artefak kuno tersebut. Mereka menjelajahi hutan yang lebat, melawan makhluk-makhluk mistis, dan memecahkan teka-teki kuno. Sementara itu, langit terus mengamuk, dan badai yang dahsyat merusak desa. Mia dan Rian menemukan petunjuk bahwa artefak pertama berada di dalam gua terlarang. Tanpa ragu, mereka memasuki gua tersebut, dihadapkan pada ujian-ujian yang mematikan.', 'Misi Pencarian', 2, 35),
+(39, 'Mia dan Rian berhasil mendapatkan dua artefak, tetapi langit semakin mengamuk. Mereka menyadari bahwa langit membutuhkan pengorbanan besar untuk meredakan kemarahannya. Keduanya memutuskan untuk menyatukan kekuatan mereka dan mengorbankan diri untuk menyelamatkan desa. Dengan penuh tekad, mereka menghadapi langit yang gelap. Pada saat yang kritis, keajaiban terjadi. Kedua artefak bersinar terang, dan langit mereda. Mia dan Rian, meskipun lelah, melihat langit yang tenang kembali. Mereka menjadi legenda dalam desa tersebut, dihormati oleh penduduk sebagai pahlawan yang mengorbankan diri demi kedamaian.', 'Perlawanan Terakhir', 3, 35);
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`id`, `genre`) VALUES
+(2, 'Action'),
+(3, 'Horror'),
+(4, 'Comedy'),
+(28, 'Romance'),
+(33, 'Fantasy'),
+(34, 'Thriller'),
+(36, 'Musical');
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(40),
+(40),
+(40),
+(40);
+
+--
+-- Dumping data for table `karya`
+--
+
+INSERT INTO `karya` (`id`, `deskripsi`, `judul`, `genre_id`, `user_id`) VALUES
+(23, 'Karya Leila S. Chudori\r\n\r\nDi tepian laut yang tak terjangkau, air biru yang tenang mengelilingi pulau-pulau misterius yang belum pernah terjamah. Angin sepoi-sepoi membawa aroma garam laut dan merayap melalui rerumputan yang beranjak dari tepi pantai.\r\n\r\nDi dasar laut, terumbu karang berwarna-warni membentuk istana alami bagi berbagai jenis ikan dan makhluk laut lainnya. Terdapat taman-taman koral yang menyajikan pemandangan indah, sebagai rumah bagi siput laut yang mengintip keluar dari cangkangnya dan ikan-ikan kecil yang bermain-main di antara cabang karang.\r\n\r\nPada waktu senja, cahaya matahari memantul di permukaan laut, menciptakan kilauan emas yang membelai ombak. Kelompok lumba-lumba meloncat-loncat dengan sukacita, menciptakan pertunjukan alam yang memukau bagi mereka yang menyaksikannya.\r\n\r\nPada malam hari, laut menjadi arena magis di mana cahaya kecil berkilauan dari plankton yang memancarkan cahaya. Ikan-ikan malam dengan warna-warna yang menggoda bergerak di antara bayang-bayang, menciptakan pertunjukan lampu alami yang menghiasi dasar laut.\r\n\r\nDi laut ini, ada cerita-cerita tak terhitung yang diceritakan oleh ombak dan terbawa oleh angin. Mungkin ada pulau tersembunyi yang hanya dapat ditemukan oleh mereka yang memiliki hati petualang, atau mungkin ada monster laut yang legendaris yang berbaring tertidur di dasar laut, menunggu untuk muncul dari kedalaman.\r\n\r\nLaut ini adalah panggung bagi petualangan yang belum pernah terbayangkan, di mana setiap gelombang mengandung misteri dan setiap terumbu karang menyimpan rahasia. Laut bercerita bukan hanya tentang air dan ombaknya, melainkan tentang dunia ajaib yang tersembunyi di dalamnya, menunggu untuk dijelajahi dan diungkapkan.', 'Laut Bercerita', 4, 15),
+(29, 'Raja meminta anak bungsunya, Lebas (Arya Saloka) untuk mencari keberadaan Jeng Yah. Pencarian inilah yang membangun cerita Gadis Kretek. Lebas berusaha mewujudkan keinginan terakhir ayahnya menemukan keberadaan Jeng Yah.\r\n\r\nDi tengah pencarian, Lebas bertemu dengan donatur koleksi museum kretek sekaligus anak dari Rukayah (Tissa Biani/Nungki Kusumastuti) bernama Arum (Putri Marino). Rukayah sendiri adalah adik Jeng Yah. Lebas dan Arum bekerja sama mencari tahu masa lalu keluarga yang selama ini tak diceritakan.\r\n\r\nJeng Yah adalah anak perempuan pemilik pabrik rokok Merdeka. Mimpinya untuk membuat saus campuran kretek sangatlah kuat. Namun, ia harus berhadapan dengan budaya patriarki di mana perempuan tak diperbolehkan memasuki ruang saus. Adanya perempuan di sana dipercaya membuat rasa saus menjadi asam.\r\n\r\nSegala halangan tak lantas membuat Jeng Yah menyerah. Ia berhasil mematahkan stereotip tersebut dengan saus buatannya. Dari situ, merk rokok kretek baru bernama Gadis pun tercipta.\r\n\r\nMimpi memiliki rokok dengan saus buatannya tak lantas membuat Jeng Yah hidup bahagia. Ia harus menerima kenyataan bahwa bisnis kretek keluarganya hancur lebur menyusul tragedi 65. Selain itu, Raja kekasihnya lantas mencuri resep kretek Jeng Yah dan menikahi Purwanti (Sheila Dara), anak dari rival usaha kretek keluarganya. ', 'Gadis Kretek', 2, 26),
+(31, '\"Ada Cinta di SMA\" merupakan kisah yang menggambarkan liku-liku kehidupan tiga remaja SMA, Iqbaal, Aldy, dan Kiki. Iqbaal, seorang murid yang tidak populer, memutuskan untuk mencalonkan diri sebagai Ketua Osis untuk membuktikan bahwa dia tidak seburuk yang dipikirkan oleh orang-orang di sekitarnya. Namun, teman baiknya, Aldy, awalnya meremehkan keputusan Iqbaal dan menganggapnya norak.\r\n\r\nMasalah semakin rumit ketika Aldy, bersama Iqbaal, mencari personil tambahan untuk band mereka dan mengajak Kiki. Kiki bersedia menjadi anggota band asalkan Aldy bisa meyakinkan Iqbaal untuk benar-benar mencalonkan diri sebagai Ketua Osis. Aldy, dalam keadaan bimbang, akhirnya menyetujui syarat Kiki.\r\n\r\nDalam perjalanan pencalonan, Iqbaal harus menghadapi persaingan sengit dengan Ayla, siswi berprestasi yang dibantu oleh Tara. Sementara Ayla mengubah penampilannya, Iqbaal memilih pendekatan persuasif melalui media sosial dan jaringan pertemanan. Namun, ketika Iqbaal mengetahui bahwa Aldy menggunakan cara curang dengan memberikan uang pada siswa untuk memilihnya, hubungan mereka pun diuji.\r\n\r\nDi samping itu, Kiki, mantan Ketua Osis, menghadapi masalah pribadi terkait hubungannya dengan ayahnya yang tidak setuju dengan minat musik Kiki. Cerita ini memperlihatkan upaya Aldy untuk memperbaiki hubungannya dengan Iqbaal dan memberikan semangat kepada Kiki. Dengan banyak konflik dan emosi yang terlibat, \"Ada Cinta di SMA\" menjadi kisah yang memperlihatkan perjuangan, persahabatan, dan pencarian identitas remaja di lingkungan sekolah.', 'Ada Cinta di SMA', 28, 25),
+(32, 'Cerita Putri Niwerigading mengisahkan tentang kehidupan penuh cobaan dan perjuangan Amat Mude, seorang putra mahkota yang harus menghadapi kekejamannya Raja Muda setelah sang ayah meninggal dunia. Dibuang ke hutan terpencil bersama ibunya, mereka menemukan keberuntungan melalui telur-telur emas dari ikan hasil pancingan Amat Mude.\r\n\r\nKetika Raja Muda mengancam Amat Mude untuk memetik kelapa gading di pulau angker demi menyembuhkan penyakit istrinya, kisah berlanjut dengan petualangan Amat Mude yang penuh ketulusan dan keberanian. Ditemani oleh makhluk-makhluk mistis seperti Si Lenggang Raye, Raja Buaya, dan seekor Naga, Amat Mude berhasil mengatasi rintangan dan bertemu dengan Putri Niwer Gading di pohon kelapa gading.\r\n\r\nDengan kecantikan Putri Niwer Gading dan cinta yang tulus, Amat Mude berhasil membawa pulang kelapa gading, membuktikan keajaiban dan keadilan. Kisah ini mencakup elemen-elemen fantasi, petualangan, dan moralitas, dan dapat dikategorikan sebagai cerita rakyat fantasi atau dongeng moral.', 'Putri Niwerigading', 2, 25),
+(35, 'Pada malam yang gelap, langit di desa kecil tiba-tiba mengamuk, mengejutkan penduduknya. Dengan petir menyambar tanpa henti, desa tersebut dihantui oleh ketakutan. Seorang tokoh misterius muncul dan memberi tahu mereka bahwa hanya dengan menemukan tiga artefak kuno, mereka dapat merestuasi kedamaian. Dua pahlawan, Mia dan Rian, dipilih untuk menjalani misi berbahaya untuk menyelamatkan desa. Dengan melibatkan pertarungan melawan makhluk-makhluk mistis dan mengungkap teka-teki kuno, mereka merintis perjalanan yang penuh tantangan. Namun, ketika langit semakin mengamuk, Mia dan Rian menyadari bahwa mereka harus membuat pengorbanan besar untuk menyelamatkan desa mereka. Dengan keputusan sulit, mereka menyatukan kekuatan dan menghadapi langit yang gelap, membawa kedamaian kembali ke desa dan meninggalkan jejak legenda di hati penduduknya.', 'Langit Mengamuk', 3, 15);
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `fullname`, `isAdmin`, `password`, `username`) VALUES
+(15, 'Minuettaro', b'1', '123', 'minuettaro'),
+(16, 'Jokowi', b'0', '123', 'jokowi'),
+(17, 'Naruto', b'0', '123', 'naruto'),
+(18, 'Skyla', b'0', '123', 'skyla'),
+(22, 'Naruto', b'0', '123', 'naruto'),
+(25, 'Regita', b'0', 'rgt', 'regtat'),
+(26, 'adila', b'0', 'adila14', 'AdilaZhr');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
